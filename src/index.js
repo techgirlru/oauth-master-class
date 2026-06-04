a805d600c76957484e4006b17a6e12e3abf10e6b
G P <36325706+techgirlru@users.noreply.github.com>
2026-06-04 11:57:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ const authorize = ({
   default_avatar_id: defaultAvatarId,
   display_name: displayName,
 }) => {
-  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/\${defaultAvatarId}/islands-middle')"></div>`;
-  const nameHtml = `<div class="name">\${displayName}</div>`;
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
 
   document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
 };
 
 // Делаем запрос за инфой о пользователе.
 const fetchYandexData = (token) =>
-  fetch(`https://login.yandex.ru/info?format=json&oauth_token=\${token}`).then(
+  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
     (res) => res.json()
   );
-
+ 
 window.onload = () => {
   document.getElementById("suggest").onclick = () => {
     YaAuthSuggest.init(
