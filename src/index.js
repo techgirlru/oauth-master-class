ba18183b0639aae81308ef79135d8cb3daf78cad
G P <36325706+techgirlru@users.noreply.github.com>
2026-06-04 11:37:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,8 +17,23 @@ const fetchYandexData = (token) =>
 
 window.onload = () => {
   document.getElementById("suggest").onclick = () => {
-    // TODO suggest
+    YaAuthSuggest.init(
+      {
+        client_id: "4c...3a",
+        response_type: "token",
+        redirect_uri: "https://oauth-master-class-chi-seven.vercel.app/token.html",
+      },
+      "https://oauth-master-class-chi-seven.vercel.app"
+    )
+    .then(({ handler }) => handler())
+    .then(async (data) => {
+      const result = await fetchYandexData(data.access_token);
+      authorize(result);
+      console.log(result, data);
+    })
+    .catch((error) => console.log("Что-то пошло не так: ", error));
   };
+};
 
   document.getElementById("button").onclick = () => {
     // TODO button
